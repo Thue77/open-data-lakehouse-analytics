b5a54e5c330b980ed5f8c7a3007936062b135155
Thomas Sørensen <thom199b@gmail.com>
2023-09-29 17:00:39 +0000

    update configuration

--- env_vars_in_files.ps1
+++ env_vars_in_files.ps1
@@ -1,21 +1,3 @@
-param(
-    [string]$Path,
-    [switch]$Verbose,
-    [switch]$Remove,
-    [switch]$RemoveQuotes
-)
-
-Write-Host "Collecting variables from conf file $Path"
-$variables = Select-String -Path $Path -Pattern '^\s*[^\s=#]+=[^\s]+$' -Raw
-
-Write-Host "Variables to be substituted: $variables"
-
-foreach($var in $variables) {
-    $keyVal = $var -split '=', 2
-    $key = $keyVal[0].Trim()
-    Write-Host "Variable to be substituted $key"
-}
-
 # Create list variable of file names in which secrets should be substituted
 $files = @(
     ".\etc\catalog\adls2.properties.temp",
